6d0e7f7f9febc08af63c3175e95393a1635a10b0
Kenji Taguchi <ktagjp@gmail.com>
2015-10-12 20:21:29 +0900

    Delete all files without CONFIG failes.

--- src/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void initial_jsready_callback();
-
-#endif
--- src/persist.h
+++
@@ -1,7 +0,0 @@
-#ifndef PERSIST_H
-#define PERSIST_H
-
-void load_persisted_values(WeatherData *weather_data);
-void store_persisted_values(WeatherData *weather_data);
-
-#endif
